0d4b1cfa30460a5beecda8c151eba9a056ba45be
Teemu <tkoivisto456@gmail.com>
2022-05-20 15:12:04 +0300

    ci: create .npmrc directly

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-gcloud artifacts print-settings npm \
-  --project=atypon-artifact \
-  --repository=npm-repo \
-  --location=us-central1 \
-  --scope=@manuscripts > .npmrc
+cat << EOF >.npmrc
+registry=https://registry.npmjs.org/
+@manuscripts:registry=https://us-central1-npm.pkg.dev/atypon-artifact/npm-repo/
+//us-central1-npm.pkg.dev/atypon-artifact/npm-repo/:always-auth=true
+EOF
 npx google-artifactregistry-auth
 export NPM_TOKEN=`grep -E '//us-central1-npm.pkg.dev/atypon-artifact.*authToken=' ~/.npmrc | tail -1 | cut -f2 -d=`
 pnpm --filter \"./quarterback-packages/**\" --no-git-checks publish
