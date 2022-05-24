7849fa41359bb642bac7886e5fc291fbe05c36d9
Teemu <tkoivisto456@gmail.com>
2022-05-24 13:20:46 +0300

    ci: create .npmrc inside Jenkins

--- publish.sh
+++ publish.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 set -e;
 
-# Remove any dangling npmrcs with perhaps incorrect registries / tokens
-if test -f ".npmrc"; then
-  rm .npmrc
-fi
-
-if test -f "~/.npmrc"; then
-  rm ~/.npmrc
-fi
-
 if [[ -z "${NPM_TOKEN}" ]]; then
   echo 'No NPM_TOKEN set!'
   exit 1
 fi
+
+clean_npmrcs() {
+  if test -f ".npmrc"; then
+    rm .npmrc
+  fi
+  if test -f "~/.npmrc"; then
+    rm ~/.npmrc
+  fi
+}
+
+trap clean_npmrcs EXIT
+
+cat << EOF >.npmrc
+//registry.npmjs.org/:_authToken=$NPM_TOKEN
+EOF
 
 pnpm ci:publish
