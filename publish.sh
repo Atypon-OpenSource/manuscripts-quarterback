e8edc7755b988b3bbb7aad0a13af93fa7f337c02
Teemu <tkoivisto456@gmail.com>
2022-05-24 12:50:30 +0300

    ci: remove .npmrc if they exist

--- publish.sh
+++ publish.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 set -e;
 
+# Remove any dangling npmrcs with perhaps incorrect registries / tokens
+if test -f ".npmrc"; then
+  rm .npmrc
+fi
+
+if test -f "~/.npmrc"; then
+  rm ~/.npmrc
+fi
+
 if [[ -z "${NPM_TOKEN}" ]]; then
   echo 'No NPM_TOKEN set!'
   exit 1
 fi
 
-pnpm --filter "./quarterback-packages/**" --no-git-checks publish
+pnpm ci:publish
