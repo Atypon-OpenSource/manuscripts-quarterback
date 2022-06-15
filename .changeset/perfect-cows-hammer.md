d8e23e8ed4e9cc926b62edf99537a81321d49024
Teemu <tkoivisto456@gmail.com>
2022-06-15 09:46:49 +0300

    fix(api): temporarily reset prod db on API launch

---
+++ .changeset/perfect-cows-hammer.md
@@ -0,0 +1,6 @@
+---
+"@manuscripts/quarterback-api": patch
+"@manuscripts/quarterback-db": patch
+---
+
+fix: temporarily reset prod DB on Quarterback API launch
--- quarterback-packages/api/start.sh
+++ quarterback-packages/api/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pnpm --filter @manuscripts/quarterback-db prod:migrate && \
+pnpm --filter @manuscripts/quarterback-db prod:reset && \
 node ./quarterback-packages/api/dist/index.js
