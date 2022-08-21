b42597ea340f967ca12eff22d6e17f79c91cf02e
Islam <imomani@atypon.com>
2022-08-21 14:40:30 +0300

    add accept/reject inline buttons

--- quarterback-packages/track-changes-plugin/src/plugin.ts
+++ quarterback-packages/track-changes-plugin/src/plugin.ts
@@ -25,6 +25,7 @@ import { fixInconsistentChanges } from './track/fixInconsistentChanges'
 import { trackTransaction } from './track/trackTransaction'
 import { updateChangeAttrs } from './track/updateChangeAttrs'
 import { TrackChangesOptions, TrackChangesState, TrackChangesStatus } from './types/track'
+import { createDecorations } from 'dom'
 
 export const trackChangesPluginKey = new PluginKey<TrackChangesState>('track-changes')
 
@@ -48,6 +49,10 @@ export const trackChangesPlugin = (
     props: {
       editable(state) {
         return trackChangesPluginKey.getState(state)?.status !== TrackChangesStatus.viewSnapshots
+      },
+
+      decorations(state) {
+        return createDecorations(state)
       },
     } as EditorProps,
     state: {
