55fcc564dacf1c1f9aa646c395b5eacc67393e13
Jiri Kaiser <jkaiser@atypon.com>
2023-10-13 14:27:52 +0200

    bugfixing

--- quarterback-packages/track-changes-plugin/src/change-steps/processChangeSteps.ts
+++ quarterback-packages/track-changes-plugin/src/change-steps/processChangeSteps.ts
@@ -128,8 +128,7 @@ export function processChangeSteps(
         : addTrackIdIfDoesntExist(trackUtils.createNewUpdateAttrs(emptyAttrs, c.node.attrs))
       // Dont add update changes if there exists already an insert change for this node
       if (
-        (JSON.stringify(oldAttrs) !== JSON.stringify(c.newAttrs) ||
-          c.node.type === c.node.type.schema.nodes.citation) &&
+        JSON.stringify(oldAttrs) !== JSON.stringify(c.newAttrs) &&
         !oldDataTracked.find(
           (d) => d.operation === CHANGE_OPERATION.insert && d.status === CHANGE_STATUS.pending
         )
